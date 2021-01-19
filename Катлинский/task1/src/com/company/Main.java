3d0e9ed958b14f493dfa5595196a11d5697444a4
Katlinski-Aliaksei <katlinskiialeks@gmail.com>
2021-01-19 12:20:29 +0300

    /

--- Катлинский/task1/src/com/company/Main.java
+++ Катлинский/task1/src/com/company/Main.java
@@ -5,10 +5,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Введите число");
-        int value = s.nextInt();
-        if (value > 7)
-            System.out.println("Привет");
+
+        System.out.println("Введите число:");
+        parser();
     }
+
+    public static void parser() {
+        Scanner s = new Scanner(System.in);
+        Double value = null;
+        boolean flag = true;
+        while (flag) {
+            try {
+                value = Double.parseDouble(s.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Некорректный ввод данных! \n Введите число:");
+                continue;
+            }
+            if (value > Double.MAX_VALUE || value < (Double.MIN_VALUE)) {
+                System.out.println("Введенное Вами число находится за пределами double \n Попробуйте ещё раз:");
+                continue;
+            }
+            flag = false;
+
+        }
+        sayHello(value);
+
+    }
+
+    public static void sayHello(double value){
+        if (value > 7) {
+            System.out.println("Привет");
+        }
+    }
+
 }
