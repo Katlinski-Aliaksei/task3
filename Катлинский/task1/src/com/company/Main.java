47ff7c60ed57c7fcc1ac663f747f594ab9ed848a
Katlinski-Aliaksei <katlinskiialeks@gmail.com>
2021-01-19 12:47:01 +0300

    /

--- Катлинский/task1/src/com/company/Main.java
+++ Катлинский/task1/src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Введите число:");
         parser();
     }
@@ -37,5 +36,4 @@ public class Main {
             System.out.println("Привет");
         }
     }
-
 }
