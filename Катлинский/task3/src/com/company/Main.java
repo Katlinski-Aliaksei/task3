8acd644413f63d6ba962b26da60f9b1134f60325
Katlinski-Aliaksei <katlinskiialeks@gmail.com>
2021-01-19 16:03:00 +0300

    /

--- Катлинский/task3/src/com/company/Main.java
+++ Катлинский/task3/src/com/company/Main.java
@@ -1,21 +1,26 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = new int[5];
-        Scanner s = new Scanner(System.in);
+        int[] array = new int[20];
+        int max = 200;
+        int min = -200;
 
+        System.out.print("Массив имеет вид: ");
         for (int i = 0; i < array.length; i++) {
-            System.out.println("Введите число " + (i + 1));
-            array[i] = s.nextInt();
-        }
-        for (int i : array) {
-            if (i % 3 == 0)
-                System.out.println(i);
+            array[i] = (int) (Math.random() * (max - min) + min);
+            System.out.print(array[i] + " ");
         }
 
+        System.out.print("\nЭлементы массива кратные 3: ");
+        for (int i : array) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
